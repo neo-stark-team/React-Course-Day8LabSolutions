2b1369905fd3d49200b072289c19a1808a408615
ponmalar <ponmalar@iamneo.ai>
2023-01-27 14:23:57 +0000

    CEL02

--- CEL/CEL02/reactapp/src/tests/App.test.js
+++ CEL/CEL02/reactapp/src/tests/App.test.js
@@ -3,13 +3,13 @@ import App from '../App';
 
 test('test_case01', () => {
   render(<App />);
-  const linkElement = screen.getByText(/fragments/i);
+  const linkElement = screen.getByText(/header/i);
   expect(linkElement).toBeInTheDocument();
 })
 
-test('test_case02', () => {
+test('test_case01', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Arabic/i);
+  const linkElement = screen.getByText(/test/i);
   expect(linkElement).toBeInTheDocument();
 })
 
